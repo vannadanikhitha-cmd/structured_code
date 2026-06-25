afbb730c44a4ca066634afd3956ceaa0face51b0
vannadanikhitha <vannadanikhitha@gmail.com>
2026-06-25 17:30:50 +0530

    key error

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-
-from app.api.routers.table_router import router
+import uvicorn
+from api.routers.table_router import router
 
 app = FastAPI()
 
 app.include_router(router)
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
